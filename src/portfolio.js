9b3b15e69573699b9180acbee8552addf8280551
Davidlespada <davidlespada@gmail.com>
2024-09-25 22:24:40 +0200

    Update js files

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "David L. Espada",
+  title: "Hi! I'm David L. Espada",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "👨‍💻 Unreal Engine & C++ Programmer | Expert in complex systems & gameplay mechanics 🎮 | Let's create something amazing! 🚀"
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
@@ -33,9 +33,9 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
+  github: "https://github.com/davidlespada",
+  linkedin: "https://www.linkedin.com/in/david-l%C3%B3pez-89177a214/",
+  gmail: "davidlespada@gmail.com",
   gitlab: "https://gitlab.com/saadpasta",
   facebook: "https://www.facebook.com/saad.pasta7",
   medium: "https://medium.com/@saadpasta",
